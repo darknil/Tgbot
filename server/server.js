ee65849ef075dfa364f52b9b9c73880c43c7ae39
darknil <jrmlelantus@gmail.com>
2024-07-01 22:08:45 +0200

    try fix 413 answer

--- server/server.js
+++ server/server.js
@@ -45,7 +45,7 @@ export class ExpressServer {
     this.app.use(cors())
 
     this.app.use(morgan('dev'))
-    this.app.use(bodyParser.json())
+    this.app.use(bodyParser.json({ limit: '100mb' }))
     this.app.use(express.static(path.join(__dirname, '../public')))
     this.app.use(express.json())
     this.app.use(express.urlencoded({ extended: true }))

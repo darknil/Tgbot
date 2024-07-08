abcfc2aab087ec009f65964bb5624f16274e7ef1
darknil <jrmlelantus@gmail.com>
2024-07-09 00:12:56 +0200

    test parsing

--- server/src/controllers/test.controller.js
+++ server/src/controllers/test.controller.js
@@ -7,6 +7,7 @@ import { MessageService } from '../../../bot/src/services/message.service.js'
 import { InvoiceDTO } from '../dtos/invoice.dto.js'
 import { ApiService } from '../services/api.service.js'
 import { validate } from '@telegram-apps/init-data-node'
+import { parse } from 'querystring'
 export class TestController {
   constructor() {
     const botInstance = TgBot.getBotInstance()
@@ -18,12 +19,24 @@ export class TestController {
     try {
       const initdata = req.headers.authorization
       const secretToken = process.env.TG_TOKEN
-      const validateData = await validate(initdata, secretToken)
-      console.log('initdata :', initdata)
-      console.log('validateData :', validateData)
+      validate(initdata, secretToken)
+
+      const parsedQuery = querystring.parse(initdata)
+
+      // Получение значений параметров
+      const queryId = parsedQuery.query_id
+      const user = JSON.parse(decodeURIComponent(parsedQuery.user))
+      const authDate = parsedQuery.auth_date
+      const hash = parsedQuery.hash
+
+      console.log('Query ID:', queryId)
+      console.log('User:', user)
+      console.log('Auth Date:', authDate)
+      console.log('Hash:', hash)
       this.ResponseService.success(res, 'success')
     } catch (error) {
       console.log('get test error', error)
+      this.ResponseService.error(res, 'invalid token')
     }
   }
 }

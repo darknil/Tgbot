b2910ae5e45b51d0eccb40da9f14bbbd8a5602cb
darknil <jrmlelantus@gmail.com>
2024-07-14 23:35:42 +0200

    fix

--- bot/src/handlers/channelHandlers/channelHandler.js
+++ bot/src/handlers/channelHandlers/channelHandler.js
@@ -5,39 +5,40 @@ export class ChannelHandler {
     this.UserService = new UserService()
     this.bot.on('new_chat_members', this.handleUserJoin.bind(this))
   }
-  async handleUserJoin(msg) {
+  async handleBotAddedToChannel(msg) {
     try {
-      if (msg.new_chat_members) {
-        const newMember = msg.new_chat_members[0]; // Предполагаем, что новый участник - первый в списке
-        if (newMember.is_bot) {
-          // Проверяем, что добавленный участник - бот
-          console.log('msg :',msg)
-          const channelId = msg.chat.id;
-          const inline_keyboard = {
-            inline_keyboard: [
-              [
-                {
-                  text: 'да',
-                  callback_data: `addChannel.${channelId}`,
-                },
-                {
-                  text: 'нет',
-                }
-              ],
+      const chat = msg.chat;
+      const newChatMember = msg.new_chat_member;
+      const status = newChatMember.status;
+
+      // Проверяем, что бот был добавлен в канал
+      if (chat.type === 'channel' && status === 'member') {
+        const channelId = chat.id; // Получаем идентификатор канала
+        const inline_keyboard = {
+          inline_keyboard: [
+            [
+              {
+                text: 'Да',
+                callback_data: `addChannel.${channelId}`,
+              },
+              {
+                text: 'Нет',
+                callback_data: 'doNothing'
+              }
             ],
-          };
-          const admins = await this.UserService.getAdmins()
-          for (let admin of admins) {
-            console.log('admin', admin)
-            await this.bot.sendMessage(admin.chatId, 'Бот был добавлен в канал', {
-              reply_markup: inline_keyboard,
-            });
-          }
-          // Здесь можно добавить логику для обработки добавления бота в канал
+          ],
+        };
+
+        const admins = await this.UserService.getAdmins();
+        for (let admin of admins) {
+          await this.bot.sendMessage(admin.chatId, 'Бот был добавлен в канал', {
+            reply_markup: inline_keyboard,
+          });
         }
+        // Здесь можно добавить логику для обработки добавления бота в канал
       }
     } catch (error) {
-      console.error('Ошибка обработки добавления бота:', error);
+      console.error('Ошибка обработки добавления бота в канал:', error);
     }
   }
 }

420ea36c379a769a30259aaf612983c0d0e3714c
darknil <jrmlelantus@gmail.com>
2024-07-14 19:28:40 +0200

    fix

--- bot/src/services/channel.service.js
+++ bot/src/services/channel.service.js
@@ -10,11 +10,7 @@ export class ChannelService {
       const chatMember = await this.bot.getChatMember(ChannelId, userChatId)
       // Check if the user is a member (returns 'member' status)
 
-      return (
-        chatMember.status === 'creator' ||
-        chatMember.status === 'member' ||
-        chatMember.status === 'administrator'
-      )
+      return chatMember.status
     } catch (error) {
       console.error('Error checking user membership:', error)
       throw error // You may handle errors differently based on your application's needs

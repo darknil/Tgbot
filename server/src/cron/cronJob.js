e76bc9b10bc89f73ce0d7331270b799bbe0e6164
darknil <jrmlelantus@gmail.com>
2024-07-18 17:19:05 +0200

    add daysBack to cron

--- server/src/cron/cronJob.js
+++ server/src/cron/cronJob.js
@@ -6,7 +6,7 @@ export function scheduleCloseReports() {
     try {
       console.log('Starting daily close reports job...')
       const closeReportsInstance = new CloseReports()
-      await closeReportsInstance.closeReports()
+      await closeReportsInstance.closeReports(1)
       console.log('Daily close reports job completed successfully.')
     } catch (error) {
       console.log('cron error :', error)

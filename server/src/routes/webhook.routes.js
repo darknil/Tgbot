9fe1ccdc2a252c53d765b4cf17f33998d6865f31
darknil <jrmlelantus@gmail.com>
2024-08-10 19:31:29 +0200

    add console log to getcourse webhook

--- server/src/routes/webhook.routes.js
+++ server/src/routes/webhook.routes.js
@@ -1,13 +1,16 @@
 import express from 'express'
 import { WebHookController } from '../controllers/webhook.controller.js'
+import { GetcourseWebhookController } from '../controllers/getcourse.webhook.js'
 export class WebHookRouter {
   constructor() {
     this.WebHookController = new WebHookController()
+    this.GetcourseWebhookController = new GetcourseWebhookController()
     this.router = express.Router()
     this.setupRoutes()
   }
   setupRoutes() {
     this.router.post('/', this.WebHookController.handleWebHook)
+    this.router.get('/getcourse', this.GetcourseWebhookController.handleWebHook)
   }
   getRouter() {
     return this.router

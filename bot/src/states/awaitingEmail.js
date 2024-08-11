ea39f201d0257d648a5e094560bd3483efe9cc75
darknil <jrmlelantus@gmail.com>
2024-08-11 20:37:26 +0200

    fix

--- bot/src/states/awaitingEmail.js
+++ bot/src/states/awaitingEmail.js
@@ -15,12 +15,30 @@ export class AwaitingEmail {
       }
       if (!userByEmail) {
         this.UserService.updateUserField(chatId, 'email', message)
+        this.UserService.updateUserField(chatId, 'state', 'default')
         this.bot.sendMessage(chatId, 'Почта Добавлена к вашему аккаунту')
+        this.bot.sendPhoto(
+          chatId,
+          'https://3123703-of06570.twc1.net/images/Frame19.png',
+          {
+            caption: 'Путь к твоей вершине начинается здесь',
+            ...keyboards.startKeyboard
+          }
+        )
       }
       if (!userById.email) {
         await this.UserService.deleteUserByEmail(message)
         this.UserService.updateUserField(chatId, 'email', message)
+        this.UserService.updateUserField(chatId, 'state', 'default')
         this.bot.sendMessage(chatId, 'Почта Добавлена к вашему аккаунту')
+        this.bot.sendPhoto(
+          chatId,
+          'https://3123703-of06570.twc1.net/images/Frame19.png',
+          {
+            caption: 'Путь к твоей вершине начинается здесь',
+            ...keyboards.startKeyboard
+          }
+        )
       }
     } catch (error) {
       console.log('proccess awaiting email error :', error)
--- server/src/controllers/getcourse.webhook.js
+++ server/src/controllers/getcourse.webhook.js
@@ -1,15 +1,24 @@
+import 'dotenv/config'
 import { ResponseService } from '../services/response.service.js'
 import { UserService } from '../services/user.service.js'
+import { MessageService } from '../../../bot/src/services/message.service.js'
+import { TgBot } from '../../../bot/bot.js'
 export class GetcourseWebhookController {
   constructor() {
+    const botInstance = TgBot.getBotInstance()
     this.ResponseService = new ResponseService()
     this.UserService = new UserService()
+    this.MessageService = new MessageService(botInstance)
   }
   handleWebHook = async (req, res) => {
     console.log('getcourse webhook', req.query)
     console.log('getcourse webhook data :', req.body)
     const email = req.query.email
     const status = req.query.status
+    const key = req.query.key
+    if (key !== process.env.secret_key) {
+      return this.ResponseService.notFound(res, 'bad request')
+    }
     if (!email || !status) {
       return this.ResponseService.badRequest(res, 'Bad request')
     }
@@ -18,11 +27,8 @@ export class GetcourseWebhookController {
       await this.UserService.createUser('', '', '', '', '', email)
       return this.ResponseService.success(res, 'ok')
     }
-    if (status === 'Завершен') {
-      await this.UserService.updateUserStatus(existedUser, 'member')
-      return this.ResponseService.success(res, 'ok')
-    }
 
+    await this.UserService.updateUserStatus(existedUser, 'member')
     return this.ResponseService.success(res, 'ok')
   }
 }
